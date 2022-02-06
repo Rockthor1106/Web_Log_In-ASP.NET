e81c4a01b878ee10fbd3560bc5c778749cee236f
Gilmar Amezquita <gilmarandresamezquita@hotmail.com>
2022-02-06 00:13:14 -0500

    Minor changes in html

---
+++ Web_Log_In-ASP.NET/Migrations/20220206035022_Init.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web_Log_In_ASP.NET.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
